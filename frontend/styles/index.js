4c6e3d6cf46ca6ae536a9dfb5578dfbd513b06e9
kil-2310 <homakov600@gmail.com>
2026-03-07 11:06:39 +0300

    updated the sass imports

--- frontend/styles/index.js
+++ frontend/styles/index.js
@@ -1,1 +1,9 @@
-import './styles.scss'
+import './normalize.css'
+import './variables.css'
+
+import './main.scss'
+import './site_header.scss'
+import './global_selectors.scss'
+import './fonts.scss'
+import './footer.scss'
+import './articles.scss'
