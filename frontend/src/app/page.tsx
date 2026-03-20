650b69e0610570a6381b850553d988e8b95e6499
kil-2310 <homakov600@gmail.com>
2026-03-20 10:30:41 +0300

    added a formatter

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,55 +1,58 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
-import EnterVideo from "@/shared/ui/EnterVideo"
-import DescriptionRegion from "@/features/description_region"
-import BlockArticles from "@/shared/ui/BlocksArticle"
+import EnterVideo from '@/shared/ui/EnterVideo';
+import DescriptionRegion from '@/features/description_region';
 
 export const metadata: Metadata = {
-  title: "Курск и Курская область на карте России: экология, экономика, история, культура, туризм",
-  description: "Курская область на карте России: подробная информация об экологии, экономике, истории, культуре и туризме региона. Достопримечательности, промышленность и природные особенности Курска.",
-  keywords: [
-    "Курск", 
-    "Курская область", 
-    "карта России", 
-    "экология Курска", 
-    "экономика Курской области", 
-    "история Курска", 
-    "культура Курска", 
-    "туризм в Курской области", 
-    "достопримечательности Курска", 
-    "природа Курской области"
-  ],
-  authors: [{ 
-    name: "Хомяков Евгений Алексеевич",
-    url: "https://kursk-region.ru"
-  }],
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
+    title: 'Курск и Курская область на карте России: экология, экономика, история, культура, туризм',
+    description:
+        'Курская область на карте России: подробная информация об экологии, экономике, истории, культуре и туризме региона. Достопримечательности, промышленность и природные особенности Курска.',
+    keywords: [
+        'Курск',
+        'Курская область',
+        'карта России',
+        'экология Курска',
+        'экономика Курской области',
+        'история Курска',
+        'культура Курска',
+        'туризм в Курской области',
+        'достопримечательности Курска',
+        'природа Курской области',
+    ],
+    authors: [
+        {
+            name: 'Хомяков Евгений Алексеевич',
+            url: 'https://kursk-region.ru',
+        },
+    ],
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+        },
     },
-  },
-  metadataBase: new URL("https://kursk-region.ru"),
-  alternates: {
-    canonical: "https://kursk-region.ru",
-  },
-  openGraph: {
-    title: "Курск и Курская область на карте России: экология, экономика, история, культура, туризм",
-    description: "Курская область на карте России: подробная информация об экологии, экономике, истории, культуре и туризме региона.",
-    url: "https://kursk-region.ru",
-    siteName: "Курская область",
-    locale: "ru_RU",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Курск и Курская область на карте России",
-    description: "Подробная информация об экологии, экономике, истории, культуре и туризме региона",
-  }
+    metadataBase: new URL('https://kursk-region.ru'),
+    alternates: {
+        canonical: 'https://kursk-region.ru',
+    },
+    openGraph: {
+        title: 'Курск и Курская область на карте России: экология, экономика, история, культура, туризм',
+        description:
+            'Курская область на карте России: подробная информация об экологии, экономике, истории, культуре и туризме региона.',
+        url: 'https://kursk-region.ru',
+        siteName: 'Курская область',
+        locale: 'ru_RU',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Курск и Курская область на карте России',
+        description:
+            'Подробная информация об экологии, экономике, истории, культуре и туризме региона',
+    },
 };
-
 
 export default function HomePage() {
     return (
@@ -57,5 +60,5 @@ export default function HomePage() {
             <EnterVideo />
             <DescriptionRegion />
         </>
-    )
+    );
 }
